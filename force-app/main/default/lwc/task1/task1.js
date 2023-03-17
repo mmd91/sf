dcb40ccf885bc88b6349748564897b1b4766d09f
mmd91 <malets.maria1@gmail.com>
2023-03-17 22:34:00 +0200

    first

---
+++ force-app/main/default/lwc/task1/task1.js
@@ -0,0 +1,32 @@
+/* Created by mariamalets on 28.02.2023.
+ */
+
+import { LightningElement, api } from 'lwc';
+import { updateRecord } from 'lightning/uiRecordApi';
+import { CloseActionScreenEvent } from 'lightning/actions';
+
+	
+import Cancel_Button from '@salesforce/label/c.Cancel_Button';
+import Save_Button from '@salesforce/label/c.Cancel_Button';
+
+
+export default class Task1 extends LightningElement {
+    @api recordId;
+    @api Account;
+
+    label = {
+      Cancel_Button,
+      Save_Button
+   };
+   
+
+    handleSuccess(event){
+        this.dispatchEvent(new CloseActionScreenEvent());
+     }
+
+     handleCancel(event){
+        this.dispatchEvent(new CloseActionScreenEvent());
+     }
+
+
+    }
---
+++ force-app/main/default/lwc/task2/task2.js
@@ -0,0 +1,28 @@
+import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
+
+import { api, LightningElement, wire } from 'lwc';
+
+import Name from '@salesforce/schema/Account.Name';
+import Phone from '@salesforce/schema/Account.Phone';
+import Costs__c from '@salesforce/schema/Account.Costs__c';
+
+export default class Task2 extends LightningElement {
+    @api recordId;
+    
+    
+    @wire(getRecord, { recordId: '$recordId', fields: [Name, Phone, Costs__c] }) account;
+
+    get accountName (){
+        return getFieldValue(this.account.data, Name);
+    }
+
+    get accountPhone (){
+        return getFieldValue(this.account.data , Phone);
+    }
+
+    get accountCosts (){
+        return getFieldValue(this.account.data , Costs__c);
+    }
+
+  
+}
