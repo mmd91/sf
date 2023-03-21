62d34fc776d63f81d111c0fa64ed5069208dcb62
mmd91 <malets.maria1@gmail.com>
2023-03-21 15:28:41 +0200

    21.03

--- force-app/main/default/lwc/task2/task2.js
+++ force-app/main/default/lwc/task2/task2.js
@@ -1,6 +1,7 @@
 import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
 
-import { api, LightningElement, wire } from 'lwc';
+import { api, LightningElement, track, wire } from 'lwc';
+import { updateRecord } from 'lightning/uiRecordApi';
 
 import Name from '@salesforce/schema/Account.Name';
 import Phone from '@salesforce/schema/Account.Phone';
@@ -9,11 +10,25 @@ import Costs__c from '@salesforce/schema/Account.Costs__c';
 export default class Task2 extends LightningElement {
     @api recordId;
     
+    // accountName;
+    // accountPhone;
+    // accountCosts;
+
     
-    @wire(getRecord, { recordId: '$recordId', fields: [Name, Phone, Costs__c] }) account;
+   @wire(getRecord, { recordId: '$recordId', fields: [Name, Phone, Costs__c] }) account;
+
+//   connectedCallback(){
+//     this.accountName = this.getFieldValue(this.account.data,  Name);
+//     this.accountPhone = this.getFieldValue(this.account.data , Phone);
+//     this.accountCosts = this.getFieldValue(this.account.data , Costs__c);
+//   }
+
+//   getFieldValue(record, field) {
+//     return record ? record.fields[field.fieldApiName].value : '';
+// }
 
     get accountName (){
-        return getFieldValue(this.account.data,  );
+        return getFieldValue(this.account.data,  Name);
     }
 
     get accountPhone (){
@@ -24,5 +39,55 @@ export default class Task2 extends LightningElement {
         return getFieldValue(this.account.data , Costs__c);
     }
 
+    handleAccountNameChange(event){
+         // Update the account name variable with the new value
+        this.accountName = event.target.value;
+    }
+
+    handleAccountPhoneChange(event){
+         // Update the account phone variable with the new value
+    this.accountPhone = event.target.value;
+    }
+
+    handleAccountCostChange(event){
+         // Update the account cost variable with the new value
+        this.accountCosts = event.target.value;
+    }
+
+    updateAccount(){
+       
+        const fields = {};
+        fields[Name.fieldApiName] = this.accountName;
+        fields[Phone.fieldApiName] = this.accountPhone;
+        fields[Costs__c.fieldApiName] = this.accountCosts;
+
+        const recordInput = { 
+            fields, 
+            id: this.recordId };
+
+            updateRecord(recordInput)
+            .then(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Account updated successfully.',
+                        variant: 'success'
+                    })
+                );
+            })
+            .catch(error => {
+                console.error('Error updating account', error);
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error',
+                        message: error.body.message,
+                        variant: 'error'
+                    })
+                );
+            });
+
+    }
+  
+    
   
 }
